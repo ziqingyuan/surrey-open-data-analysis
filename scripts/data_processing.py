adfd964b400dfacf60e9362ba1e937889f2c74c2
zoeyzq <zoeyzq@students.cs.ubc.ca>
2023-02-26 23:06:31 -0800

    finished clean data, implement vidualization

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,13 +1,23 @@
+import csv
 import json
 import os
 import sys
+
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 import string
-import model
+import pandas as pd
 from typing import List, Any
-from pyproj import Transformer, Proj, transform
-from model import TransitStopRaw, TransitStop
+import pyproj
+from model import TransitStop, Restaurant
+
+
+class GeoSystem:
+    WGS84 = "EPSG:4326"
+    UTM_SURREY = "EPSG:26910"
+
+
+DATA_PATH = "../data/"
 
 
 def load_transit_data():
@@ -22,7 +32,7 @@ def load_transit_data():
         Returns:
             A python array, each is a transit stop info
     """
-    transit_file = open('../data/raw/transit.json')
+    transit_file = open(DATA_PATH + 'raw/transit.json')
     transit_data = json.load(transit_file)
     return transit_data[2].get('features')
 
@@ -53,7 +63,7 @@ def get_stop_property(stop: Any, property_name: string):
     return stop.get('properties').get(property_name)
 
 
-def parse_to_raw_stop_data(transit_stop_array: List[TransitStopRaw]):
+def json_to_stop_data(transit_stop_array: List[TransitStop]):
     stops_raw_data = load_transit_data()
     for stop in stops_raw_data:
         if "Point" != stop.get('geometry').get('type'):
@@ -64,31 +74,113 @@ def parse_to_raw_stop_data(transit_stop_array: List[TransitStopRaw]):
         stop_location = stop_property.get('LOCATION')
         stop_x = stop_coordinates[0]
         stop_y = stop_coordinates[1]
-        stop_raw = TransitStopRaw(
+        lat, lon = map_system_converter(GeoSystem.UTM_SURREY, GeoSystem.WGS84, stop_x, stop_y)
+        stop_raw = TransitStop(
             id=stop_id,
-            location=stop_location,
-            x=stop_x,
-            y=stop_y
+            city=stop_location,
+            utm_x=stop_x,
+            utm_y=stop_y,
+            lon=lon,
+            lat=lat
+
         )
         transit_stop_array.append(stop_raw)
 
 
-def convert_to_latlon(raw_data_arr: List[TransitStopRaw],
-                      parsed_arr: List[TransitStop],
-                      lon_arr: List[float],
-                      lat_arr: List[float]):
-    wgs84_sys = Proj("EPSG:4326")
-    utm_surrey_sys = Proj("EPSG:26910")
-    for stop_raw in raw_data_arr:
-        x = stop_raw.x
-        y = stop_raw.y
-        lon, lat = transform(utm_surrey_sys, wgs84_sys, x, y)
-        lon_arr.append(lon)
-        lat_arr.append(lat)
-        stop_cleaned = TransitStop(
-            id=stop_raw.id,
-            location=stop_raw.location,
-            lon=lon,
-            lat=lat
-        )
-        parsed_arr.append(stop_cleaned)
+def create_restaurant(row):
+    tracking_number = row[0]
+    name = row[1]
+    city = row[2]
+    address = row[3]
+    lon = row[4]
+    lat = row[5]
+    x, y = map_system_converter(GeoSystem.WGS84, GeoSystem.UTM_SURREY, lat, lon)
+    new_restaurant = Restaurant(
+        tracking_number=tracking_number,
+        name=name,
+        city=city,
+        address=address,
+        lon=lon,
+        lat=lat,
+        utm_x=x,
+        utm_y=y)
+    return new_restaurant
+
+
+def csv_to_restaurant_data(restaurant_array: List[Restaurant]):
+    restaurant_data = pd.read_table(DATA_PATH + "raw/restaurants.csv", sep=',')
+    zip_data = zip(restaurant_data['TRACKINGNUMBER'],
+                   restaurant_data['NAME'],
+                   restaurant_data['PHYSICALCITY'],
+                   restaurant_data['PHYSICALADDRESS'],
+                   restaurant_data['LONGITUDE'],
+                   restaurant_data['LATITUDE'])
+    for row in zip_data:
+        restaurant_array.append(create_restaurant(row))
+
+
+def map_system_converter(orig_sys: string, to_sys: string, x: float, y: float):
+    """
+        Covert a geological coordinate from original system to target system
+
+        Args:
+            orig_sys: a string represent original system name
+            to_sys: a string represent target system name
+            x: the first value in the coordinate, longitude for lon-lat system, x for utm system
+            y: the second value in the coordinate, latitude for lon-lat system, y for utm system
+
+        Returns:
+            A tuple of float depend on the target system
+    """
+    transformer = pyproj.Transformer.from_crs(orig_sys, to_sys)
+
+    x_after, y_after = transformer.transform(x, y)
+    return x_after, y_after
+
+
+def generate_clean_restaurant(restaurant_array: List[Restaurant]):
+    # Define the fieldnames for the CSV file
+    fieldnames = ["TRACKINGNUMBER", "NAME", "CITY", "ADDRESS", "LONGITUDE", "LATITUDE", "UTMX", "UTMY"]
+
+    # Create a new CSV file in write mode
+    with open(DATA_PATH + "processed/restaurant.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write the header row to the CSV file
+        writer.writeheader()
+
+        # Write each person as a row in the CSV file
+        for restaurant in restaurant_array:
+            writer.writerow({
+                "TRACKINGNUMBER": restaurant.tracking_number,
+                "NAME": restaurant.name,
+                "CITY": restaurant.city,
+                "ADDRESS": restaurant.address,
+                "LONGITUDE": restaurant.lon,
+                "LATITUDE": restaurant.lat,
+                "UTMX": restaurant.utm_x,
+                "UTMY": restaurant.utm_y
+            })
+
+
+def generate_clean_transit_stop(transit_stop_array: List[TransitStop]):
+    # Define the fieldnames for the CSV file
+    fieldnames = ["ID", "CITY", "LONGITUDE", "LATITUDE", "UTMX", "UTMY"]
+
+    # Create a new CSV file in write mode
+    with open(DATA_PATH + "processed/transit_stops.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write the header row to the CSV file
+        writer.writeheader()
+
+        # Write each person as a row in the CSV file
+        for transit_stop in transit_stop_array:
+            writer.writerow({
+                "ID": transit_stop.id,
+                "CITY": transit_stop.city,
+                "LONGITUDE": transit_stop.lon,
+                "LATITUDE": transit_stop.lat,
+                "UTMX": transit_stop.utm_x,
+                "UTMY": transit_stop.utm_y
+            })
--- scripts/model.py
+++ scripts/model.py
@@ -4,39 +4,57 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class TransitStopRaw:
-    def __init__(self, id, x, y, location):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.location = location
-
-
 class TransitStop(Base):
+    """
+            A class representing a TransitStop.
+
+            Attributes:
+            - id (str): The BUS_STOP_NO
+            - lon (float): The bus stop's longitude
+            - lat (float): The bus stop's latitude
+            - location (str): The BUS_STOP_NO
+            - utm_x (float): The bus stop's coordinates under UTM system
+            - utm_y (float): The bus stop's coordinates under UTM system
+    """
     __tablename__ = 'transit_stops'
 
     id = Column(Integer, primary_key=True)
     lon = Column(Float)
     lat = Column(Float)
-    location = Column(String)
+    city = Column(String)
+    utm_x = Column(Float)
+    utm_y = Column(Float)
 
-    def __init__(self, id, lon, lat, location):
+    def __init__(self, id, lon, lat, city, utm_x, utm_y):
         self.id = id
         self.lon = lon
         self.lat = lat
-        self.location = location
+        self.city = city
+        self.utm_x = utm_x
+        self.utm_y = utm_y
 
 
 class Restaurant(Base):
-    __tablename__ = 'transit_stops'
+    __tablename__ = 'restaurant'
 
-    id = Column(Integer, primary_key=True)
+    tracking_number = Column(String, primary_key=True)
     lon = Column(Float)
     lat = Column(Float)
-    location = Column(String)
+    name = Column(String)
+    city = Column(String)
+    address = Column(String)
+    utm_x = Column(Float)
+    utm_y = Column(Float)
 
-    def __init__(self, id, lon, lat, location):
-        self.id = id
+    def __init__(self, tracking_number, name, lon, lat, city, address, utm_x, utm_y):
+        self.tracking_number = tracking_number
+        self.name = name
         self.lon = lon
         self.lat = lat
-        self.location = location
+        self.city = city
+        self.address = address
+        self.utm_x = utm_x
+        self.utm_y = utm_y
+
+
+
